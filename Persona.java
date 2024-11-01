36f5e669bdf0d5a8d3abad562311b35f18acc79c
patito7777 <andreaherrerauni@gmail.com>
2024-11-01 10:00:00 -0400

    Añadir Persona

---
+++ Persona.java
@@ -0,0 +1,6 @@
+﻿public class Persona {
+    private String nombre;
+    private String apellido;
+    public Persona(String n, String a){ this.nombre = n; this.apellido = a; }
+    public String nombreCompleto(){ return nombre + " " + apellido; }
+}
