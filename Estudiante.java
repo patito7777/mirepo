85db26606364e8fa711f3ef2d84ed7ba7c3d9641
patito7777 <andreaherrerauni@gmail.com>
2024-11-02 11:00:00 -0400

    Añadir Estudiante

---
+++ Estudiante.java
@@ -0,0 +1,5 @@
+﻿public class Estudiante extends Persona {
+    private int legajo;
+    public Estudiante(String n, String a, int l){ super(n,a); this.legajo = l; }
+    public int getLegajo(){ return legajo; }
+}
