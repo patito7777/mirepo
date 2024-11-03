be8619e59fd3decfd780374aa6592c483da99694
patito7777 <andreaherrerauni@gmail.com>
2024-11-03 12:00:00 -0400

    Añadir Docente

---
+++ Docente.java
@@ -0,0 +1,5 @@
+﻿public class Docente extends Persona {
+    private String departamento;
+    public Docente(String n, String a, String d){ super(n,a); this.departamento = d; }
+    public String getDepartamento(){ return departamento; }
+}
