efadddce6c558716f82d09453006adb5114c8b9f
patito7777 <andreaherrerauni@gmail.com>
2024-11-04 13:00:00 -0400

    Añadir Main

---
+++ Main.java
@@ -0,0 +1,8 @@
+﻿public class Main {
+    public static void main(String[] args){
+        Estudiante e = new Estudiante("Ana","Perez",123);
+        Docente d = new Docente("Carlos","Lopez","Matematicas");
+        System.out.println(e.nombreCompleto() + " - " + e.getLegajo());
+        System.out.println(d.nombreCompleto() + " - " + d.getDepartamento());
+    }
+}
